dbc8be650c6bea9b594e908041a20630a2a4be6f
Arif suat Kivam <akivam@k1m35s01.42istanbul.com.tr>
2025-06-01 10:01:13 +0300

    norm hataları düzeltilip kullanılmayan kütüphaneler kaldırıldı

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akivam <akivam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/01 09:59:45 by akivam            #+#    #+#             */
+/*   Updated: 2025/06/01 10:00:20 by akivam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include"libft.h"
-#include<stdio.h>
-#include <string.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char *str = (char *)s;
-	size_t i;
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
 	i = 0;
 	while (str[i] != '\0')
-		i++;
+		i = i + 1;
 	while (str[--i])
 	{
 		if (str[i] == c)
@@ -16,13 +28,5 @@ char	*ft_strrchr(const char *s, int c)
 			return (str + i);
 		}
 	}
-	return 0;
-}
-
-int main()
-{
-	char *a = "merhakba kdünyak";
-	char s = 'k';
-	printf("%s \n",ft_strrchr(a,s));
-	return 0;
+	return (0);
 }
