3fad90e0f92451b24359f1b0460e5966878d0163
Arif suat Kivam <akivam@k1m35s01.42istanbul.com.tr>
2025-06-01 08:47:04 +0300

    test kodları ve test için olan kütüphaneler kaldırıldı

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,12 +6,11 @@
 /*   By: akivam <akivam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 09:01:04 by akivam            #+#    #+#             */
-/*   Updated: 2025/05/30 10:42:06 by akivam           ###   ########.fr       */
+/*   Updated: 2025/06/01 08:45:29 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
-#include <stdio.h>
 
 int	ft_isdigit(int c)
 {
@@ -19,21 +18,3 @@ int	ft_isdigit(int c)
 		return (1);
 	return (0);
 }
-/*
-int main(int argc, char const *argv[])
-{
-	int a = ' ';
-	int b = '0';
-	int c = '9';
-	int d = 'a';
-	int e = 'b';
-	int f = '10';
-	printf("%d",ft_isdigit(a));
-	printf("%d",ft_isdigit(b));
-	printf("%d",ft_isdigit(c));
-	printf("%d",ft_isdigit(d));
-	printf("%d",ft_isdigit(e));
-	printf("%d",ft_isdigit(f));
-	return 0;
-}
-*/
