0926e49a67d596a921993d1841168eab426df1b6
Arif suat Kivam <akivam@k1m35s01.42istanbul.com.tr>
2025-06-01 09:47:28 +0300

    test kodları ve kullanılmayan kütüphaneler kaldırıldı

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,12 @@
 /*   By: akivam <akivam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 11:29:15 by akivam            #+#    #+#             */
-/*   Updated: 2025/05/31 17:51:42 by akivam           ###   ########.fr       */
+/*   Updated: 2025/06/01 09:46:58 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-/* this function gives pointer array lenght*/
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
@@ -21,12 +20,4 @@ size_t	ft_strlen(const char *s)
 	while (s[i] != '\0')
 		i = i + 1;
 	return (i);
-}
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-	char s[] = "deneme";
-	printf("%zu",ft_strlen(s));
-	return (0);
 }
