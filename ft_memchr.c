fb7cf4452c0e0ce59d12e3ecf79069ee5c519bfa
Arif suat Kivam <akivam@k1m35s01.42istanbul.com.tr>
2025-06-01 08:54:55 +0300

    test kodları ve kullanılmayan kütüphaneler silindi

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,32 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akivam <akivam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/01 08:49:17 by akivam            #+#    #+#             */
+/*   Updated: 2025/06/01 08:54:13 by akivam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include"libft.h"
-
-
 
 void	*ft_memchr(const void *s1, int c, size_t n)
 {
-	size_t i;
-	unsigned char *str = (unsigned char*) s1;
+	size_t			i;
+	unsigned char	*str;
 
+	str = (unsigned char *) s1;
 	while (str[i])
 	{
 		if (str[i] == c && i < n)
-			return(str + i);
+			return (str + i);
 		i++;
 	}
-	return NULL;
-	
-}
-#include <stdio.h>
-#include <string.h>
-
-int main(int argc, char const *argv[])
-{
-	char *a = "merhaba dünya";
-	char b = 'd';
-	char* s = ft_memchr(a,b,9);
-	char* f = memchr(a,b,9);
-	printf("%s",s);
-	printf("\n");
-	printf("%s",f);
-	return 0;
+	return (NULL);
 }
