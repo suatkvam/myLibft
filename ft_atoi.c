d7973fe92c2c88cd9a65abbb3ecac93508fed08e
suatkvam <suatkivam@gmail.com>
2025-06-15 15:42:06 +0300

    atoinin parametreleri düzeltildi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: akivam <akivam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 21:33:18 by akivam            #+#    #+#             */
-/*   Updated: 2025/06/15 14:31:05 by akivam           ###   ########.fr       */
+/*   Updated: 2025/06/15 15:41:44 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(char *nptr)
+int	ft_atoi(const char *nptr)
 {
 	int	i;
 	int	sign;
