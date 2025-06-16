bd309b9424371acca604d3268dce845352552e0c
Sora <34161147+suatkvam@users.noreply.github.com>
2025-06-16 18:26:16 +0300

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,31 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akivam <akivam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/01 09:27:47 by akivam            #+#    #+#             */
-/*   Updated: 2025/06/15 14:31:36 by akivam           ###   ########.fr       */
+/*   Created: 2025/06/01 08:55:35 by akivam            #+#    #+#             */
+/*   Updated: 2025/06/16 18:21:14 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char	*str;
-	size_t	i;
+	size_t			i;
+	unsigned char	*tmp_src;
+	unsigned char	*tmp_dst;
 
-	str = (char *)s;
 	i = 0;
-	while (str[i] != '\0')
+	tmp_src = (unsigned char *)src;
+	tmp_dst = dest;
+	if ((tmp_src == NULL && tmp_dst == NULL) && n > 0)
+		return (NULL);
+	while (i < n)
 	{
-		if ((unsigned char)(str[i]) == (unsigned char)c)
-			return (str + i);
+		tmp_dst[i] = tmp_src[i];
 		i++;
 	}
-	if ((unsigned char)c == '\0')
-		return (str + i);
-	return (NULL);
+	return (dest);
 }
